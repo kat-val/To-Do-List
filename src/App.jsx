9acf6de4783225e4b9084af956952cb6816c81aa
Kat-val <valentk17@gmail.com>
2024-03-13 18:00:39 -0300

    Actualizando codigo

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,18 @@ function App() {
 
 	return (
 		<>
-			<main className='p-4 flex flex-col'>
-				<div className="flex flex-col gap-4">
-					<h2 className='text-lg poppins-regular'>Lista de tareas</h2>
-					<span className="border-b border-neutral-700"></span>
-				</div>
-				
-				<TaskList/>
-
-				<div className="flex flex-col gap-4">
-					<h2 className='text-lg poppins-regular'>Terminadas</h2>
-					<span className="border-b border-neutral-700"></span>
-				</div>
-			</main>
+			<TaskList/>
 		</>
 	)
 }
 export default App
 
-{/* <nav className="">
+{/*
+<nav className="">
 	<ul className="flex gap-5">
 		<li><a className="font-medium hover:text-violet-300 transition-colors duration-200 ease-out" href="#">Por hacer</a></li>
 		<span className="text-violet-400">/</span>
 		<li><a className=" hover:text-violet-300 transition-colors duration-200 ease-out" href="#">Terminadas</a></li>
 	</ul>
-</nav> */}
+</nav>
+*/}
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -3,10 +3,72 @@ import { TaskItem } from "./TaskItem"
 export const TaskList = () => {
    return (
       <>
-         <section className='flex flex-col gap-5 mt-6 mb-10'>
-            <TaskItem/>
-            <TaskItem/>
-         </section>
+         <main className='p-4 flex flex-col gap-10'>
+				<section className="flex flex-col gap-5">
+               <div className="flex flex-col gap-4">
+                  <h2 className='text-lg poppins-regular'>Lista de tareas</h2>
+                  <span className="border-b border-neutral-700"></span>
+               </div>
+               <TaskItem/>
+            </section>
+
+
+				<section className="flex flex-col gap-5">
+               <div className="flex flex-col gap-4">
+                  <h2 className='text-lg poppins-regular'>Terminadas</h2>
+                  <span className="border-b border-neutral-700"></span>
+               </div>
+               <TaskItem/>
+            </section>
+			</main>
       </>
    )
 }
+
+// **. / App.jsx**
+// ```jsx
+// const Card = () =>{
+// 	const productos = [
+// 		{
+// 			"title": 'Samsung',
+// 			"price": '14500',
+// 			"condition": 'new',
+// 			"free_shipping": true,
+// 			"thumbnail": 'https://url/img.jpg'
+// 		},
+// 		// ...
+// 	]
+
+// return(
+// 	<div className='container-cards'>
+// 		{productos.map(product =>
+// 			<Card price={product.price} 
+// 				shipping={product.free_shipping} 
+// 				title={product.title} 
+// 				condition={product.condition} 
+// 				img={product.thumbnail}
+// 			/>
+// 		)}
+// 	</div>
+// )}
+// ```
+
+// **. / Component.jsx**
+// ```jsx
+// const Card = ({title,condition,img,shipping,price}) =>{
+
+// return(
+// 	<div>
+// 		<img src={img} alt=''/>
+// 		<div>
+// 			<h4>{title}</h4>
+// 			<p>Condición: {condition}</p>
+// 			<p>Precio: ${price}</p>
+
+// 			{ shipping && 
+// 				<p> ¡Envio gratis! </p> 
+// 				<!-- Si su condición es True, se muestra -->
+// 			}
+// 		</div>
+// 	</div>
+// )}
