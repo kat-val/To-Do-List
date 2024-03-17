8f83ef64c522afcdef4839b89d70d3552daee175
Kat-val <valentk17@gmail.com>
2024-03-17 14:10:29 -0300

    Cambio de logica para cambiar clases al checkearlas (a medio funcionar)

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 // import PropTypes from "prop-types";
 
-export const TaskItem = ({condicion}) => {
+export const TaskItem = ({text,todoTask,todosTask,setTodosTask}) => {
    const [toggleClass, setToggleClass]= useState(false);
    const [viewIcons, setViewIcons]= useState(false);
-   const [checkElement,setCheckElement]= useState(false);
+   // const [checkElement,setCheckElement]= useState(false);
    
    
    const activateIconsEdit =()=>{
@@ -12,55 +12,82 @@ export const TaskItem = ({condicion}) => {
       setToggleClass(!toggleClass);
    }
    const checkItem = ()=>{
-      setCheckElement(!checkElement);
-      condicion(!checkElement);
+      // setCheckElement(!checkElement);
+
+      setTodosTask(todosTask.map((item) =>{
+         if(item.id === todoTask.id){
+            return {
+               ...item,
+               checked: !item.checked,
+            };
+         }
+         console.log(item.checked);
+         return item;
+      }));
    }
 
-   let title = "Check Box";
-   let description = "Ipsum perferendis officiis velit nihil accusantium, tempora et? 🔥";
+   const deleteTask =()=>{
+      setTodosTask(todosTask.filter(item => item.id !== todoTask.id));
+      // Iteramos sobre cada elemento, y va eliminar el que cumpla la función (tiene id diferente al especificado).
+   }
 
    return (
       <>
-         {checkElement ?
-            <div className='flex flex-col gap-5'>
-               <div className="flex items-center gap-4">
-                  <label className='relative flex flex-1 px-5 py-4 rounded-lg outline outline-1 bg-gradient-to-br from-neutral-800 outline-neutral-700'>
-                     <div className="flex flex-col gap-1 ml-4 pl-5 text-neutral-500 line-through">
-                        <h4 className="text-base poppins-regular">{title}</h4>
-                        <p className='text-sm'>{description}</p>
-                     </div>
-                     <input className="hidden" type="checkbox" name="check" onClick={checkItem}/>
-                     <span className="checkmark cursor-pointer absolute top-5 left-5 h-5 w-5 bg-neutral-500 rounded-full transition-all duration-200 ease-linear after:content-[''] after:absolute after:hidden"></span>
-                  </label>
-                  <div className="flex flex-col items-center gap-3 text-neutral-500">
-                     <span className="h-3 w-3 bg-violet-400 rounded-sm hover:bg-neutral-500 transition-colors duration-200 ease-out"></span>
-                     <i className="bi bi-trash3 text-lg text-neutral-500 hover:text-red-400 transition-colors duration-200 ease-out"></i>
+            <div className="flex items-center gap-4">
+               <label className='relative flex flex-1 px-6 py-5 rounded-lg outline outline-1 bg-gradient-to-br from-neutral-800 outline-neutral-700'>
+                  <div className="flex flex-col gap-1 ml-4 pl-5 text-neutral-500 line-through">
+                     <h4 className="text-base poppins-regular">{text}</h4>
                   </div>
+                  <input className="hidden" type="checkbox" name="check" onClick={checkItem}/>
+                  <span className="checkmark cursor-pointer absolute top-[21px] left-6 h-5 w-5 bg-neutral-500 rounded-full transition-all duration-200 ease-linear after:content-[''] after:absolute after:hidden"></span>
+               </label>
+               <div className="flex flex-col items-center justify-center text-neutral-500">
+                  <span className="h-3 w-3 bg-violet-400 rounded-sm hover:bg-neutral-500 transition-colors duration-200 ease-out my-2"></span>
+                  <i className="bi bi-trash3 text-lg text-neutral-500 hover:text-red-400 transition-colors duration-200 ease-out" onClick={deleteTask}></i>
                </div>
             </div>
-            :
-            <div className='flex flex-col gap-5'>
-               <div className="flex items-center gap-4">
-                  <label className='relative flex flex-1 px-5 py-4 rounded-lg outline outline-1 bg-neutral-700  gap-5 outline-neutral-600'>
-                     <div className="flex flex-col gap-1 ml-4 pl-5">
-                        <h4 className="text-base poppins-regular">{title}</h4>
-                        <p className='text-sm text-neutral-400'>{description}</p>
-                     </div>
-                     <input className="hidden" type="checkbox" name="check" onClick={checkItem}/>
-                     <span className="checkmark cursor-pointer absolute top-5 left-5 h-5 w-5 bg-neutral-500 rounded-full transition-all duration-200 ease-linear after:content-[''] after:absolute after:hidden"></span>
-                  </label>
-                  <div className="flex flex-col items-center gap-3 h-full flex-wrap text-neutral-500 relative">
-                     <i className={`${toggleClass ? 'bi-caret-left-fill absolute right-7 top-5 hover:text-violet-400 text-neutral-400':'bi-three-dots-vertical text-neutral-500 hover:text-violet-400'} bi text-lg cursor-pointer  transition-all duration-300 ease-out`} onClick={activateIconsEdit}></i>
-                     {viewIcons &&
-                        <>
-                           <i className="bi bi-pencil-square hover:text-violet-400 transition-colors duration-200 ease-out text-lg"></i>
-                           <i className="bi bi-trash3 hover:text-red-400 transition-colors duration-200 ease-out text-lg"></i>
-                        </> 
-                     }
+            
+            <div className="flex items-center gap-4">
+               <label className='relative flex flex-1 px-6 py-5 rounded-lg outline outline-1 bg-neutral-700  gap-5 outline-neutral-600'>
+                  <div className="flex flex-col gap-1 ml-4 pl-5">
+                     <h4 className="text-base poppins-regular">{text}</h4>
                   </div>
+                  <input className="hidden" type="checkbox" name="check" onClick={checkItem}/>
+                  <span className="checkmark cursor-pointer absolute top-[21px] left-6 h-5 w-5 bg-neutral-500 rounded-full transition-all duration-200 ease-linear after:content-[''] after:absolute after:hidden"></span>
+               </label>
+               <div className="flex flex-col items-center gap-2 h-full flex-wrap text-neutral-500 relative">
+                  <i className={`${toggleClass ? 'bi-caret-left-fill absolute right-9 top-5 hover:text-violet-400 text-neutral-400':'bi-three-dots-vertical text-neutral-500 hover:text-violet-400'} bi text-lg cursor-pointer  transition-all duration-300 ease-out`} onClick={activateIconsEdit}></i>
+                  {viewIcons &&
+                     <>
+                        <i className="bi bi-pencil-square hover:text-violet-400 transition-colors duration-200 ease-out text-lg"></i>
+                        <i className="bi bi-trash3 hover:text-red-400 transition-colors duration-200 ease-out text-lg" onClick={deleteTask}></i>
+                     </> 
+                  }
                </div>
             </div>
-         }
       </>
    )
 }
+
+
+            // <div className="flex items-center gap-4">
+            //    <label className={`relative flex flex-1 px-6 py-5 rounded-lg outline outline-1 gap-5 ${todoTask.checked ? "bg-gradient-to-b from-neutral-800 outline-neutral-700" : "bg-neutral-700 outline-neutral-600"}`}>
+            //       <div className={`flex flex-col gap-1 ml-4 pl-5 ${todoTask.checked ? "text-neutral-500 line-through" : ""}`}>
+            //          <h4 className="text-base poppins-regular">{text}</h4>
+            //       </div>
+            //       <input className="hidden" type="checkbox" name="check" onClick={checkItem}/>
+            //       <span className="checkmark cursor-pointer absolute top-[21px] left-6 h-5 w-5 bg-neutral-500 rounded-full transition-all duration-200 ease-linear after:content-[''] after:absolute after:hidden"></span>
+            //    </label>
+            //    <div className={`flex flex-col items-center ${todoTask.checked ? "justify-center text-neutral-500" : "gap-2 h-full flex-wrap text-neutral-500 relative"}`}>   
+            //       <i className={`${toggleClass ? 'bi-caret-left-fill absolute right-9 top-5 hover:text-violet-400 text-neutral-400':'bi-three-dots-vertical text-neutral-500 hover:text-violet-400'} bi text-lg cursor-pointer  transition-all duration-300 ease-out`} onClick={activateIconsEdit}></i>
+            //       {viewIcons &&
+            //          <>
+            //             <i className="bi bi-pencil-square hover:text-violet-400 transition-colors duration-200 ease-out text-lg"></i>
+            //             <i className="bi bi-trash3 hover:text-red-400 transition-colors duration-200 ease-out text-lg" onClick={deleteTask}></i>
+            //          </> 
+            //       }
+
+            //       {/* <span className="h-3 w-3 bg-violet-400 rounded-sm hover:bg-neutral-500 transition-colors duration-200 ease-out my-2"></span>
+            //       <i className="bi bi-trash3 text-lg text-neutral-500 hover:text-red-400 transition-colors duration-200 ease-out" onClick={deleteTask}></i> */}
+            //    </div>
+            // </div>
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,24 +1,33 @@
 // import { TaskItem } from "./TaskItem"
 
 import { useState } from "react";
+import { TaskItem } from "./TaskItem";
 
 export const TaskList = () => {
-   // const componenteCheckeado = false;
-   const [toggleClass, setToggleClass]= useState(false);
-   const [viewIcons, setViewIcons]= useState(false);
-   const [checkElement,setCheckElement]= useState(false);
+   const [todosTask, setTodosTask]= useState([]);
    
-   const activateIconsEdit =()=>{
-      setViewIcons(!viewIcons);
-      setToggleClass(!toggleClass);
+   const [inputTask, setInputTask]= useState("");
+   // const [outputValues,setOutputValues]= useState(false);
+
+   const inputTaskText =(e)=>{
+      setInputTask(e.target.value);
    }
-   const checkItem = ()=>{
-      setCheckElement(!checkElement);
-      // condicion(!checkElement);
+   const submitTask =(e)=>{
+      e.preventDefault();
+      // setOutputValues(true);
+      
+      setTodosTask([...todosTask, {
+         text: inputTask,
+         checked: false,
+         id: Math.random()*1000
+      }]);
+
+      setInputTask("");
    }
 
-   let title = "Check Box";
-   let description = "Ipsum perferendis officiis velit nihil accusantium, tempora et? 🔥";
+   // let title = "Terminar proyecto To-Do-List";
+   // let description = "¿Se podrá terminar con esta tortura? El bagre no se rinde 🔥";
+   // let description = "Ipsum perferendis officiis velit nihil accusantium, tempora et? 🔥";
 
    return (
       <>
@@ -29,52 +38,30 @@ export const TaskList = () => {
                   <span className="border-b border-neutral-700"></span>
                </div>
 
-               {checkElement ?
-                  <div className='flex flex-col gap-5'>
-                     <div className="flex items-center gap-4">
-                        <label className='relative flex flex-1 px-5 py-4 rounded-lg outline outline-1 bg-gradient-to-br from-neutral-800 outline-neutral-700'>
-                           <div className="flex flex-col gap-1 ml-4 pl-5 text-neutral-500 line-through">
-                              <h4 className="text-base poppins-regular">{title}</h4>
-                              <p className='text-sm'>{description}</p>
-                           </div>
-                           <input className="hidden" type="checkbox" name="check" onClick={checkItem}/>
-                           <span className="checkmark cursor-pointer absolute top-5 left-5 h-5 w-5 bg-neutral-500 rounded-full transition-all duration-200 ease-linear after:content-[''] after:absolute after:hidden"></span>
-                        </label>
-                        <div className="flex flex-col items-center gap-3 text-neutral-500">
-                           <span className="h-3 w-3 bg-violet-400 rounded-sm hover:bg-neutral-500 transition-colors duration-200 ease-out"></span>
-                           <i className="bi bi-trash3 text-lg text-neutral-500 hover:text-red-400 transition-colors duration-200 ease-out"></i>
-                        </div>
-                     </div>
-                  </div>
-                  :
-                  <div className='flex flex-col gap-5'>
-                     <div className="flex items-center gap-4">
-                        <label className='relative flex flex-1 px-5 py-4 rounded-lg outline outline-1 bg-neutral-700  gap-5 outline-neutral-600'>
-                           <div className="flex flex-col gap-1 ml-4 pl-5">
-                              <h4 className="text-base poppins-regular">{title}</h4>
-                              <p className='text-sm text-neutral-400'>{description}</p>
-                           </div>
-                           <input className="hidden" type="checkbox" name="check" onClick={checkItem}/>
-                           <span className="checkmark cursor-pointer absolute top-5 left-5 h-5 w-5 bg-neutral-500 rounded-full transition-all duration-200 ease-linear after:content-[''] after:absolute after:hidden"></span>
-                        </label>
-                        <div className="flex flex-col items-center gap-3 h-full flex-wrap text-neutral-500 relative">
-                           <i className={`${toggleClass ? 'bi-caret-left-fill absolute right-9 top-5 hover:text-violet-400 text-neutral-400':'bi-three-dots-vertical text-neutral-500 hover:text-violet-400'} bi text-lg cursor-pointer  transition-all duration-300 ease-out`} onClick={activateIconsEdit}></i>
-                           {viewIcons &&
-                              <>
-                                 <i className="bi bi-pencil-square hover:text-violet-400 transition-colors duration-200 ease-out text-lg"></i>
-                                 <i className="bi bi-trash3 hover:text-red-400 transition-colors duration-200 ease-out text-lg"></i>
-                              </> 
-                           }
-                        </div>
-                     </div>
-                  </div>
-               }
+               <form>
+                  <label className="flex gap-2">
+                     <input className="flex-1 px-3" type="text" placeholder="Agregar tarea..." value={inputTask} onChange={inputTaskText}/>
+                     <button className="py-1 px-3 text-lg bg-neutral-600" type="submit" onClick={submitTask}>+</button>
+                  </label>
+               </form>
+
+               
+               <ul className='flex flex-col gap-5'>
+                  {todosTask.map(todoTask =>(
+                     <TaskItem setTodosTask={setTodosTask} todosTask={todosTask} todoTask={todoTask} key={todoTask.id} text={todoTask.text} checked={todoTask.checked}/>
+                  ))}
+               </ul>
+               {/* {outputValues && } */}
+               
 
             </section>
 			</main>
       </>
    )
 }
+
+{/* <p className='text-sm text-neutral-400'>{description}</p> */}
+{/* <p className='text-sm'>{description}</p> */}
 
 // **. / App.jsx**
 // ```jsx
